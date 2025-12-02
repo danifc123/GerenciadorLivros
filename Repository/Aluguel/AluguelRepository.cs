05583135498c053202a2547d93d3e3d9d5ef0bc7
danifc123 <danielcarmo29@gmail.com>
2025-12-02 15:26:21 -0300

    Feat: Service and Repository

---
+++ Repository/Aluguel/AluguelRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFModels.DBConfig;
+using Models.Entities;
+
+namespace Repository.AluguelRepository
+{
+    public class AluguelRepository
+    {
+        #region Constructor
+        private readonly GerenciadorLivrariaDb _context;
+        public AluguelRepository(GerenciadorLivrariaDb context)
+        {
+            _context = context;
+        }
+        #endregion
+        #region Methods
+        public async Task<Aluguel> GetByIdAsync(int id)
+        {
+            var aluguelBanco = await _context.Alugueis.FindAsync(id);
+            
+            if (aluguelBanco == null)
+                return null;
+
+            return aluguelBanco;
+        }
+        public async Task<Aluguel> AddAsync(Aluguel aluguel)
+        {
+            await _context.Alugueis.AddAsync(aluguel);
+            await  _context.SaveChangesAsync();
+            return aluguel;
+        }
+        public async Task<Aluguel> PostAsync(int id, Aluguel aluguel) 
+        {
+            var aluguelBanco = await _context.Alugueis.FindAsync(id);
+
+            
+            aluguelBanco.AlunoId = aluguel.AlunoId;
+            aluguelBanco.LivroId = aluguel.LivroId;
+            aluguelBanco.ProfessorId = aluguel.ProfessorId;
+            aluguelBanco.DataInicio = aluguel.DataInicio;
+            aluguelBanco.DataDevolucao = aluguel.DataDevolucao;
+
+            _context.Alugueis.Update(aluguelBanco);
+            await _context.SaveChangesAsync();
+
+            return aluguelBanco;
+        }
+        public async Task<Aluguel> DeleteAsync(int id)
+        {
+            var aluguelBanco = _context.Alugueis.Find(id);
+
+            _context.Alugueis.Remove(aluguelBanco);
+            await _context.SaveChangesAsync();
+
+            return aluguelBanco;
+        }
+        #endregion
+
+
+    }
+}
---
+++ Service/Aluguel/AluguelService.cs
@@ -0,0 +1,35 @@
+﻿using Models.Entities;
+using Repository.AluguelRepository;
+
+
+namespace Service.AluguelService
+{
+    public class AluguelService
+    {
+        private readonly AluguelRepository _repository;
+
+        #region Constructor
+        public AluguelService(AluguelRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Aluguel> GetById(int id) 
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+        public async Task<Aluguel> Add(Aluguel aluguel)
+        {
+            return await _repository.AddAsync(aluguel);
+        }
+        public async Task<Aluguel> Post(int id, Aluguel aluguel) 
+        {
+            return await _repository.PostAsync(id, aluguel);
+        }
+        public async Task<Aluguel> Delete(int id) 
+        {
+            return await _repository.DeleteAsync(id);
+        }
+        #endregion
+    }
+}
