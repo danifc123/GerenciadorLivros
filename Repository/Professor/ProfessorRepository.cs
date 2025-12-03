917ec7c68b992a12f40cd9d7ce839fa6f3d16e56
danifc123 <danielcarmo29@gmail.com>
2025-12-03 14:20:59 -0300

    Fix: Professor ajustado

---
+++ Repository/Professor/ProfessorRepository.cs
@@ -0,0 +1,22 @@
+﻿using EFModels.DBConfig;
+using Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.ProfessorRepository
+{
+    public class ProfessorRepository
+    {
+        private readonly GerenciadorLivrariaDb _context;
+
+        public ProfessorRepository(GerenciadorLivrariaDb context) 
+        {
+            _context = context;
+        }
+        
+
+    }
+}
