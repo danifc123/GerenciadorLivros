4c06a2c70a8471667bbef416b786054bac21b18b
danifc123 <danielcarmo29@gmail.com>
2025-12-04 12:09:52 -0300

    FEAT: professor service

---
+++ Service/Professor/ProfessorService.cs
@@ -0,0 +1,36 @@
+﻿using Models.Entities;
+using Repository.ProfessorRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.ProfessorService
+{
+    public class ProfessorService
+    {
+        private readonly ProfessorRepository _repository;
+
+        public ProfessorService(ProfessorRepository repository) 
+        {
+            _repository = repository;
+        }
+        public async Task<Professor> GetProfessorById(int id) 
+        {
+            return await _repository.GetByIdProfessor(id);
+        }
+        public async Task<Professor> AddProfessor(int id, Professor professor) 
+        {
+            return await _repository.CreateProfessor(id, professor);
+        }
+        public async Task<Professor> AtualizaProfessor(int id, Professor professor) 
+        {
+            return await _repository.UpdateProfessor(id, professor);
+        }
+        public async Task<Professor> DeleteProfessor(int id) 
+        {
+            return await _repository.DeleteProfessor(id);
+        }
+    }
+}
