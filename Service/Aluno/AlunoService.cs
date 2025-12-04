4741deae2fdd0b717fb3321cb931fd6fbc7c1b8c
danifc123 <danielcarmo29@gmail.com>
2025-12-04 12:09:35 -0300

    FEAT: alunoservice

---
+++ Service/Aluno/AlunoService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Entities;
+using Repository.Alunos;
+
+namespace Service.AlunoService
+{
+    public class AlunoService
+    {
+        private readonly AlunoRepository _repository;
+
+        public AlunoService(AlunoRepository repository) 
+        {
+            _repository = repository;
+        }
+        public async Task<Aluno> GetById(int id)
+        {
+            return await _repository.GetAlunoByIdAsync(id);
+        }
+        public async Task<Aluno> AddAluno(Aluno aluno) 
+        {
+            return await _repository.AddAlunoAsync(aluno);
+        }
+        public async Task<Aluno> UpdateAluno(int id, Aluno aluno) 
+        {
+            return await _repository.PostAlunoAsync(id, aluno);
+        }
+        public async Task<Aluno> DeleteAluno(int id) 
+        {
+            return await _repository.DeleteAsync(id);
+        }
+    }
+}
