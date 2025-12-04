450da0f3723df28cd7f71eee44b94f636744c648
danifc123 <danielcarmo29@gmail.com>
2025-12-04 12:09:43 -0300

    FEAT: livro service

---
+++ Service/Livro/LivroService.cs
@@ -0,0 +1,38 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Entities;
+using Repository.Livros;
+
+
+namespace Service.LivroService
+{
+    public class LivroService
+    {
+        private readonly LivroRepository _repository;
+
+        public LivroService(LivroRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Livro> GetById(int id) 
+        {
+            return await _repository.GetLivroAsync(id);
+        }
+        public async Task<Livro> AdicionarLivro(Livro livro) 
+        {
+            return await _repository.AddLivroAsync(livro);
+        }
+        public async Task<Livro> AtualizarLivro(int id, Livro livro)
+        {
+            return await _repository.PostLivroAsync(id, livro);
+        }
+        public async Task<Livro> DeletarLivro(int id) 
+        {
+            return await _repository.DeleteLivroAsync(id);
+        }
+    }
+}
