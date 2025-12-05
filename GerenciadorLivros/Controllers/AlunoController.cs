793d79a91a9578fef8eb028fa2c736378119c46f
danifc123 <danielcarmo29@gmail.com>
2025-12-05 14:08:40 -0300

    Fix: aluno controller

--- GerenciadorLivros/Controllers/AlunoController.cs
+++ GerenciadorLivros/Controllers/AlunoController.cs
@@ -1,6 +1,74 @@
-﻿namespace GerenciadorLivros.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.Entities;
+using Service.AlunoService;
+using System.Linq.Expressions;
+
+namespace GerenciadorLivros.Controllers
 {
-    public class AlunoController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlunoController: ControllerBase
     {
+        private readonly AlunoService _service;
+
+        public AlunoController(AlunoService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Aluno>> GetAlunoById(int id)
+        {
+            try
+            {
+                var alunoBanco = await _service.GetById(id);
+                return Ok(alunoBanco);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut]
+        public async Task<ActionResult<Aluno>> PutAluno(int id, Aluno aluno) 
+        {
+            try
+            {
+                var alunoBanco = await _service.UpdateAluno(id, aluno);
+                return Ok(alunoBanco);
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult<Aluno>> PostAluno(Aluno aluno)
+        {
+            try
+            {
+                var alunoBanco = await _service.AddAluno(aluno);
+
+                return Ok(alunoBanco);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Aluno>> DeleteAluno(int id) 
+        {
+            try 
+            {
+                var alunoBanco = await _service.DeleteAluno(id);
+                return Ok(alunoBanco);
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
