30f6c81e6a0d57837798e4fed52c89b43c171227
danifc123 <danielcarmo29@gmail.com>
2025-12-05 14:08:58 -0300

    fix: Livro Controller

--- GerenciadorLivros/Controllers/LivroController.cs
+++ GerenciadorLivros/Controllers/LivroController.cs
@@ -1,6 +1,72 @@
-﻿namespace GerenciadorLivros.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.Entities;
+using Service.LivroService;
+
+namespace GerenciadorLivros.Controllers
 {
-    public class LivroController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LivroController: ControllerBase
     {
+        private readonly LivroService _livroService;
+
+        public LivroController(LivroService livroService)
+        {
+            _livroService = livroService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Livro>> GetLivroById(int id) 
+        {
+            try 
+            {
+               var livroBanco = await _livroService.GetById(id);
+                return Ok(livroBanco);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult<Livro>> AdicionarLivro(Livro livro) 
+        {
+            try 
+            {
+                var livroBanco = await _livroService.AdicionarLivro(livro);
+                return Ok(livroBanco);
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut]
+        public async Task<ActionResult<Livro>> AtualizarLivro(int id, Livro livro) 
+        {
+            try 
+            {
+                var livroBanco = await _livroService.AtualizarLivro(id, livro);
+                return Ok(livroBanco);
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Livro>> DeleteLivro(int id)
+        {
+            try 
+            {
+                var livroBanco = await _livroService.DeletarLivro(id);
+                return Ok(livroBanco);
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
     }
 }
